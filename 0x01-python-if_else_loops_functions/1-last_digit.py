2eb51d65569a4c816a1c648f4bd9be8af9639f52
Humphrey <kinotihum@gmail.com>
2023-10-03 04:27:00 +0000

    fixed some pycodestyle validation errors

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,10 +9,11 @@ if number < 0:
     number *= -1
 else:
     last_digit = number % 10
-    
+# Check the next condition
+message = "and is less than 6 and not 0"
 if (last_digit > 5):
     print(f"Last digit of {number} is {last_digit} and is greater than 5")
 elif (last_digit == 0):
     print(f"Last digit of {number} is {last_digit} and is 0")
 else:
-    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
+    print(f"Last digit of {number} is {last_digit} " + message)
