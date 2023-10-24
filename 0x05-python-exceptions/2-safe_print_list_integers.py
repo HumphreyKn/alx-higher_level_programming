4111c9b8c2f37a45380a906d0045ccf170bd12b8
Humphrey <kinotihum@gmail.com>
2023-10-24 01:38:07 +0000

    removed thr IndexError exception

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -9,7 +9,7 @@ def safe_print_list_integers(my_list=[], x=0):
             if isinstance(element, int):
                 print("{:d}".format(element), end="")
                 printed_elements += 1
-    except (IndexError, ValueError, TypeError):
+    except (ValueError, TypeError):
         pass
     finally:
         print()
