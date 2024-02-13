8adde1b11b67676ffea4f74d1f5e0ad7702b1ea5
HumphreyKn <kathurimahumphreyz@gmail.com>
2024-02-13 19:01:04 +0300

    creates a table called first_table in the current database

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,7 @@
+-- creates a table called first_table in the current database
+CREATE TABLE IF NOT EXISTS `first_table` (
+    -- set the id column to integer
+    `id` INT,
+    -- set the name to varchar
+    `name` VARCHAR(256)
+);
