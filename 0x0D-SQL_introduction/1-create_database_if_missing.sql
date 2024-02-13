a572bb5264b54047b5bcc222dec3f4bf6620a7a1
HumphreyKn <kathurimahumphreyz@gmail.com>
2024-02-13 18:14:14 +0300

    a script that creates the database hbtn_0c_0

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,5 @@
+-- creates the database hbtn_0c_0, and it ddeletes
+-- drop the database if it already exists
+DROP DATABASE IF EXISTS `hbtn_0c_0`;
+-- create the database
+CREATE DATABASE `hbtn_0c_0`;
