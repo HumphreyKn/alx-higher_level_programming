d4efe543b08284f832b73bd66f3d36fab38c1c93
HumphreyKn <kathurimahumphreyz@gmail.com>
2024-02-14 19:15:40 +0300

    [Fix] removed extra '-' in comment

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,5 @@
 -- uses the hbtn_0d_tvshows database to lists all genres of the show Dexter
-    --- The tv_shows table contains only one record where title = Dexter (but the id can be different)
+    -- The tv_shows table contains only one record where title = Dexter (but the id can be different)
     -- Each record should display: tv_genres.name
     -- Results must be sorted in ascending order by the genre name
 
@@ -8,4 +8,4 @@ FROM `tv_genres`
 JOIN `tv_show_genres` ON `tv_show_genres`.`genre_id` = `tv_genres`.`id`
 JOIN `tv_shows` ON `tv_show_genres`.`show_id` = `tv_shows`.`id`
 WHERE `tv_shows`.`title` = 'Dexter'
-ORDER BY `tv_genres`.`name`;
+ORDER BY `tv_genres`.`name`;    
