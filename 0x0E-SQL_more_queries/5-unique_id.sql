8b765ed9d7c22afded40883b81231e858b3b685e
HumphreyKn <kathurimahumphreyz@gmail.com>
2024-02-14 13:51:14 +0300

    [Fix]: trpo errors

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,7 +1,7 @@
 -- creates the table unique_id
     -- id INT with the default value 1 and must be unique
     -- name VARCHAR(256)
-CREAT DATABASE IF NOT EXIST `unique_id`(
+CREATE DATABASE IF NOT EXISTS `unique_id`(
     `id` INT DEFAULT 1 UNIQUE,
     `name` VARCHAR(256)
 );
